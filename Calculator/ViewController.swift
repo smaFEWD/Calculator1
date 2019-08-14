94b10e2440eb734ab367be064934e3072882edeb
Sandi Ma <sandima.job@gmail.com>
2019-08-13 17:57:05 -0700

    adding and appending numbers to display label

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
-    
+    private var isFinishedTypingNumber : Bool = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+            isFinishedTypingNumber == true
     
     }
 
@@ -24,7 +25,17 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        // currentTitle is a get only, and can't be set, it also is a String? which means it's an optional String, and it "may be nil" which can crash the app. We need to handle it in an if statement.
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber == true {
+                displayLabel.text = numValue
+                isFinishedTypingNumber == false
+            } else { // isFinishedTypingNumber == false
+                displayLabel.text = displayLabel.text! + numValue // appending addl values into the display label
+            }
+            
+        }
+        
     }
 
 }
